9ae4ac82d22255682d0452a5a60512d8ea33e3c8
SushiAbuse <justine.tran@mavs.uta.edu>
2023-03-23 11:36:46 -0500

    Revert "Revert "login-work-in-progress""
    This reverts commit b8261c532edc86289dab8df47d65e89f62c6d2dc.

--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -11,16 +11,7 @@ namespace databaseteam18
 {
 
 
-    public partial class Login
+    public partial class About
     {
-
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }

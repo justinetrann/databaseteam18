7fba53cc82f6aef0c3bd5dfbd1e964f5be9399bf
SushiAbuse <justine.tran@mavs.uta.edu>
2023-03-23 23:19:07 -0500

    gridview fixed

--- Projects_Database.aspx
+++ Projects_Database.aspx
@@ -1,8 +1,12 @@
 ﻿<%@ Page Title="Project Management System" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Projects_Database.aspx.cs" Inherits="databaseteam18.About"%>
 
+
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 <link href="Styles/project.css" rel="stylesheet" type="text/css"/>
     <main aria-labelledby="project_database">
         <p>Current Projects in Database</p>
+        <asp:GridView ID ="GridView1" AutoGenerateColumns="true" runat="server" Width="100%"  ViewStateMode="Enabled">
+
+        </asp:GridView>
     </main>
 </asp:Content>
--- Projects_Database.aspx.cs
+++ Projects_Database.aspx.cs
@@ -1,33 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace databaseteam18
 {
     public partial class Projects : System.Web.UI.Page
     {
+        protected GridView GridView1;
         protected void Page_Load(object sender, EventArgs e)
         {
             // Establishing connection string to database
             // Reading from the web.config file
-            //string dbConnectionString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+            string dbConnectionString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
 
-            //var queryString = "SELECT * FROM Projects"; // Return all records from Project Table in Database
-            //var dbConncetion = new SqlConnection(dbConnectionString);
-            //var dataAdapter = new SqlDataAdapter(queryString, dbConncetion);
+            var queryString = "SELECT * FROM Projects"; // Return all records from Project Table in Database
+            var dbConncetion = new SqlConnection(dbConnectionString);
+            var dataAdapter = new SqlDataAdapter(queryString, dbConncetion);
 
-            //var commandBuilder = new SqlCommandBuilder(dataAdapter);
-            //var ds = new DataSet();
-            //dataAdapter.Fill(ds);
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
 
-            //GridView1.DataSource = ds.Tables[0];
-            //GridView1.DataBind();
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
         }
     }
 }

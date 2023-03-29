706290eac8be6fe85560e3851f38e4ee93753ba6
Ahmed <coolmohhi@gmail.com>
2023-03-29 02:21:51 -0500

    t

---
+++ employee_task_database.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="Project Management System" Language="C#" MasterPageFile="~/employee_site.Master" AutoEventWireup="true" CodeBehind="employee_task_database.aspx.cs" Inherits="databaseteam18.employee_task_database" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <link rel="stylesheet" href="Styles/project.css">
+    <main aria-labelledby="task_database">
+        <p>Current Tasks in Database</p>
+        <asp:GridView ID ="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" Width="100%" Height ="400px">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" ForeColor="White" Font-Bold="True" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+
+
+           <Columns>
+           <%-- <asp:BoundField DataField="Id" HeaderText="ID" ReadOnly="true" />--%>
+            <asp:TemplateField HeaderText="Action">
+                <ItemTemplate>
+                    <asp:Label ID="StatusLabel" runat="server" Text='<%# Eval("Status") %>'></asp:Label>
+                </ItemTemplate>
+                <EditItemTemplate>
+                    <asp:DropDownList ID="StatusDropDownList" runat="server">
+                        <asp:ListItem Text="Start" Value="0"></asp:ListItem>
+                        <asp:ListItem Text="Pause" Value="1"></asp:ListItem>
+                        <asp:ListItem Text="Complete" Value="2"></asp:ListItem>
+                    </asp:DropDownList>
+                </EditItemTemplate>
+            </asp:TemplateField>
+            <asp:CommandField ShowEditButton="true" />
+        </Columns>
+
+        </asp:GridView>
+    </main>
+</asp:Content>
---
+++ employee_task_database.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace databaseteam18
+{
+    public partial class employee_task_database : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Establishing connection string to database
+            // Reading from the web.config file
+            int project_id = -1;
+            string dbConnectionString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+
+            SqlConnection connection = new SqlConnection(dbConnectionString);
+
+            connection.Open();
+            string employee_id = Session["employee_id"].ToString();
+
+
+            string read_project_id_query = "SELECT DISTINCT COMPANY.task_assignment.project_ID FROM COMPANY.task_assignment WHERE employee_ID = @employee_id;";
+
+            SqlCommand read_project_id_command = new SqlCommand(read_project_id_query, connection);
+            read_project_id_command.Parameters.AddWithValue("@employee_id", employee_id);
+
+
+
+
+            SqlDataReader project_id_reader = read_project_id_command.ExecuteReader();
+
+
+
+
+
+            while (project_id_reader.Read())
+            {
+                project_id = Convert.ToInt32(project_id_reader["project_ID"].ToString());
+            }
+
+            project_id_reader.Close();
+            connection.Close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //var queryString = "SELECT * FROM COMPANY.tasks";
+            var queryString  = "SELECT COMPANY.tasks.task_ID as 'Task ID', task_name as 'Task Name', task_description as 'Description',task_est_duration as 'Duration', COMPANY.task_assignment.task_status as 'Status', task_creation_date as 'Creation Date', convert(varchar,COMPANY.task_assignment.employee_id) + ' ' + employee_first_name + ' ' + employee_last_name as 'Employee'  FROM COMPANY.tasks  inner join COMPANY.task_assignment on COMPANY.task_assignment.task_id = COMPANY.tasks.task_ID inner join COMPANY.employees on COMPANY.employees.employee_id = COMPANY.task_assignment.employee_ID WHERE  COMPANY.tasks.project_ID="+ project_id +"AND COMPANY.task_assignment.employee_ID ="+employee_id; // Return all records from Project Table in Database; // Return all records from Project Table in Database
+            var dbConncetion = new SqlConnection(dbConnectionString);
+            var dataAdapter = new SqlDataAdapter(queryString, dbConncetion);
+
+            var commandBuilder = new SqlCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+
+            GridView1.DataSource = ds.Tables[0];
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            BindGridView();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values["Id"]);
+            DropDownList statusDropDownList = (DropDownList)GridView1.Rows[e.RowIndex].FindControl("StatusDropDownList");
+            int status = Convert.ToInt32(statusDropDownList.SelectedValue);
+
+            // Update the status column in the database using the id value
+            // ...
+
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            BindGridView();
+        }
+
+        private void BindGridView()
+        {
+            // Connect to database and execute query to retrieve data
+            string connectionString = "your connection string here";
+            string query = "SELECT Id, Status FROM YourTableNameHere";
+
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                adapter.Fill(dataTable);
+            }
+
+            // Bind the DataTable to the GridView control
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

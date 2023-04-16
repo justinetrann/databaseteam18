31a1d99c6febdc80581090cf379fd780b97f3ebe
Demba Diallo <dmdiallo@CougarNet.UH.EDU>
2023-04-16 17:57:26 -0500

    commit

--- Site.Master.designer.cs
+++ Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace databaseteam18
     {
 
         /// <summary>
+        /// navbarDropdownMenuLink control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor navbarDropdownMenuLink;
+
+        /// <summary>
         /// signoutButton control.
         /// </summary>
         /// <remarks>
